681fdd10a914466c2443ef1a9247619b6dfc9d5a
Mike Ems <mike@mvied.com>
2019-07-17 12:34:56 -0500

    - Tweaked `script_time` variable's weight in custom scenes.

--- script.js
+++ script.js
@@ -11,11 +11,15 @@ var bassTimevar = new Timer();
 var timevar = new Timer();
 
 function update(dt) {
-    time_bass += 0.05*(inputs.syn_BassLevel + inputs.syn_BassLevel*inputs.syn_BassLevel)*inputs.rate_in;
-    time_highs += 0.1*(inputs.syn_HighHits + inputs.syn_HighHits*inputs.syn_HighHits)*inputs.rate_in;
+    var rate_in = inputs.rate_in;
+    if (inputs.zoom >= 1 || inputs.zoom <= -1) {
+        rate_in += (inputs.zoom * 0.1);
+    }
+    time_bass += 0.05*(inputs.syn_BassLevel + inputs.syn_BassLevel*inputs.syn_BassLevel)*rate_in;
+    time_highs += 0.1*(inputs.syn_HighHits + inputs.syn_HighHits*inputs.syn_HighHits)*rate_in;
 
-    bassTimevar.updateTime(0.1, (inputs.syn_BassLevel*2.0+inputs.syn_BassPresence+inputs.syn_BassHits*2.0)*inputs.rate_in, dt);
-    timevar.updateTime(0.1, inputs.rate_in, dt);
+    bassTimevar.updateTime(0.1, (inputs.syn_BassLevel*2.0+inputs.syn_BassPresence+inputs.syn_BassHits*2.0)*rate_in, dt);
+    timevar.updateTime(0.4, rate_in, dt);
 
     uniforms.time_bass = time_bass;
     uniforms.time_highs = time_highs;
