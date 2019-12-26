4ea24c2a802bc7026afbb9ee991decd89022986e
Mike Ems <mike@mvied.com>
2019-12-26 09:40:43 -0600

    - Updated reactive shaders to be able to enable/disable reactivity smoothly.

--- main.glsl
+++ main.glsl
@@ -3,12 +3,11 @@
 #define 	twpi  	6.283185307179586  	// two pi, 2*pi
 #define 	pi   	3.141592653589793 	// pi
 
-#define time TIME
-
 vec4 renderMainImage() {
 	vec4 fragColor = vec4(0.0);
 	vec2 fragCoord = _xy;
 
+    vec2 uv = fragCoord.xy / RENDERSIZE.xy;
     vec2 v = (_xy.xy / RENDERSIZE.xy) + RENDERSIZE.y;
     v.y -= 0.5;
 
@@ -16,19 +15,19 @@ vec4 renderMainImage() {
     uvc.x += positionXY.x;
     uvc.y += positionXY.y;
 
-    float position_time = mix(script_time*0.5, bass_time*4.0, reactive_time);
-    float perspective_time = mix(script_time*0.5, bass_time*4.0, reactive_time);
+    float position_time = bass_time*4.0;
+    float perspective_time = bass_time*4.0;
     if (position == 1) {
-        uvc.x += sin(position_time*(position_momentum.x*0.1));
-        uvc.y += sin(position_time*(position_momentum.y*0.1));
+        uvc.x += (position_momentum.x)*sin(position_time);
+        uvc.y += (position_momentum.y)*sin(position_time);
     }
 
     float th = v.y * pi,
         ph = v.x * twpi;
     vec3 sp = vec3( sin(ph) * cos(th), sin(th), cos(ph) * cos(th) );
-    vec3 pos = vec3( pi, 0, 0);
+    vec3 pos = vec3( pi, rotate * PI, 0);
     pos *= zoom;
-    sp = mix(sp, normalize(vec3(uvc, 1.0)), max_perspective * (perspective == 1 ? sin(perspective_time) : 1));
+    sp = mix(sp, normalize(vec3(uvc, 1.0)), (perspective == 1 ? (min_perspective * sin(perspective_time)) * max_perspective : min_perspective));
 
     sp.yz = _rotate(sp.yz, lookXY.y*PI);
     sp.xy = _rotate(sp.xy, lookXY.x*PI);
--- script.js
+++ script.js
@@ -12,13 +12,13 @@ var timevar = new Timer();
 
 function update(dt) {
     var rate_in = inputs.rate_in;
-    if (inputs.zoom >= 1 || inputs.zoom <= -1) {
+    /*if (inputs.zoom >= 1 || inputs.zoom <= -1) {
         rate_in += (inputs.zoom * 0.1);
-    }
+    }*/
     time_bass += 0.05*(inputs.syn_BassLevel + inputs.syn_BassLevel*inputs.syn_BassLevel)*rate_in;
     time_highs += 0.1*(inputs.syn_HighHits + inputs.syn_HighHits*inputs.syn_HighHits)*rate_in;
 
-    bassTimevar.updateTime(0.1, (inputs.syn_BassLevel*2.0+inputs.syn_BassPresence+inputs.syn_BassHits*2.0)*rate_in, dt);
+    bassTimevar.updateTime(0.1, inputs.reactive_time ? (inputs.syn_BassLevel*2.0+inputs.syn_BassPresence+inputs.syn_BassHits*2.0)*rate_in : rate_in, dt);
     timevar.updateTime(0.4, rate_in, dt);
 
     uniforms.time_bass = time_bass;
